d5cf813d57505c6a6f593a9e3ad14a94044094c8
Przemyslaw Gajda <quermit@gmail.com>
2012-08-25 22:49:20 +0100

    added projects app (skeleton)

---
+++ pike/projects/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from pike.projects.models import Project
+
+
+admin.site.register(Project)
+
--- pike/projects/models.py
+++ pike/projects/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class License(models.Model):
+    name = models.CharField(max_length=64)
+    url = models.URLField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+
 class Project(models.Model):
     name = models.CharField(max_length=128)
+    description = models.TextField()
+    url = models.URLField()
+    license = models.ForeignKey(License)
+    tags = models.ManyToManyField(Tag, related_name="projects+")
 
