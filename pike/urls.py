5d2570dca838d0d0cfdb2719263d53676ec6cfe2
Przemyslaw Gajda <quermit@gmail.com>
2012-08-25 19:10:25 +0100

    Added twitter bootstrap framework for easier front-end development

--- pike/urls.py
+++ pike/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import ListView
 
 # Uncomment the next two lines to enable the admin:
@@ -16,3 +17,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
